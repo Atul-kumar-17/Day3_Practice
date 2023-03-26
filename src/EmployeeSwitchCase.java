2fa4cfc8d3cbf5e3bfcafd9e485b593c4c045f6d
Atul <atul43653@gmail.com>
2023-03-26 22:46:38 +0530

    new

--- src/EmployeeSwitchCase.java
+++ src/EmployeeSwitchCase.java
@@ -6,19 +6,19 @@ public class EmployeeSwitchCase {
         int fullTimeEmp=2;
         int empHrs=0;
         int wagePerHrs=20;
-        int randomCheck=(int)Math.floor(Math.random() * 10) % 4;
+        int randomCheck=(int)Math.floor(Math.random() * 10) % 3;
         switch (randomCheck){
-            case 1:
+            case 0:
                 System.out.println("Employee is Part time");
                 empHrs=4;
                 System.out.println("Part time employee daily wage = "+wagePerHrs*4);
                 break;
-            case 2:
+            case 1:
                 System.out.println("Employee is full time");
                 empHrs=8;
                 System.out.println("Full time employee daily wage = "+wagePerHrs*8);
                 break;
-            case 3:
+            case 2:
                 System.out.println("Employee is absent");
                 System.out.println("dailywage = "+wagePerHrs*0);
                 break;
